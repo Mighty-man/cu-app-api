33f69e034fff1ed8f7ab96eb03f2ac94312dda62
TANTOLUWA ELIZABETH ADEKOYA <tantoluwa.adekoya@gmail.com>
2018-10-04 15:09:02 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://BackendDatabase:Backend123@ds223253.mlab.com:23253/myproject');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+module.exports = app;
